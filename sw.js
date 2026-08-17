b27df788cc6f3b8d5f93c859498a1c14ee52d55d
gadicohen11 <gadi@optomise.co.za>
2026-08-17 17:29:38 +0200

    Captain Asher v11 - controls fix, clearer spikes, flag completes level, 5 hearts, emblem fix

---
+++ sw.js
@@ -0,0 +1,5 @@
+var CACHE='captain-asher-v11';
+var ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png'];
+self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}))});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.map(function(x){if(x!==CACHE)return caches.delete(x)}))}).then(function(){return self.clients.claim()}))});
+self.addEventListener('fetch',function(e){if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(function(r){var c=r.clone();caches.open(CACHE).then(function(x){x.put(e.request,c)});return r}).catch(function(){return caches.match(e.request).then(function(h){return h||caches.match('./index.html')})}))});
